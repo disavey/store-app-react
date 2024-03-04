8406e4ee319c39bbe086455c8d94d44851e1ca65
YoshuaGES <auhsoywatchandlearn@gmail.com>
2024-03-04 17:01:52 +0700

    add to cart feature

--- src/App.jsx
+++ src/App.jsx
@@ -63,6 +63,7 @@ function App() {
       ...cart,
       {
         id: cartId.id,
+        imageUrl: cartId.image,
         name: cartId.name,
         price: cartId.price,
       },
@@ -75,45 +76,56 @@ function App() {
         <h1 className="text-3xl font-semibold text-center p-7 mt-5 font-serif">
           Allam Thrift
         </h1>
-
-        <div className="flex justify-center">
-          {cart.length == 0 ? (
-            <ListCart />
-          ) : (
-            <>
-              {cart.map((carts) => (
-                <div>
-                  <table>
-                    <thead>
-                      <tr>
-                        <th>Name Product</th>
-                        <td>:</td>
-                        <td>{carts.name}</td>
-                      </tr>
-                    </thead>
-                  </table>
-                </div>
-              ))}
-            </>
-          )}
-        </div>
       </div>
       <div className="p-5">
         <div className="flex justify-between mx-10 mb-10">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
-            />
-          </svg>
+          <div className="flex gap-5">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+              />
+            </svg>
+            <div>
+              {cart.length == 0 ? (
+                <div className="text-center  w-48">
+                  <h1 className="text-lg font-medium">cart is empty</h1>
+                </div>
+              ) : (
+                <>
+                  {cart.map((carts) => (
+                    <div className="border border-slate-400" key={carts.id}>
+                      <table>
+                        <thead>
+                          <tr>
+                            <td>{carts.image}</td>
+                          </tr>
+                          <tr>
+                            <th>Name Product</th>
+                            <td>:</td>
+                            <td>{carts.name}</td>
+                          </tr>
+                          <tr>
+                            <th>Price</th>
+                            <td>:</td>
+                            <td>{carts.price}</td>
+                          </tr>
+                        </thead>
+                      </table>
+                    </div>
+                  ))}
+                </>
+              )}
+            </div>
+          </div>
           <div className="flex" onClick={handleOnClick}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -157,7 +169,7 @@ function App() {
                 harga={data.price}
                 onClickDelete={() => handleDelete(data.id)}
                 onClickEdit={() => handleEdit(data.id)}
-                onClickBag={() => handleBag(data.id)}
+                onClickBag={() => handleBag(data)}
               />
             );
           })}
@@ -166,5 +178,4 @@ function App() {
     </>
   );
 }
-
 export default App;
--- src/component/ListCart.jsx
+++ src/component/ListCart.jsx
@@ -1,8 +1,15 @@
-function ListCart() {
+function ListCart({ image }) {
   return (
     <>
-      <div className="border border-gray-300 p-2 rounded-lg w-1/2 text-center">
+      <div className="border border-gray-300 p-2 rounded-lg w-96 text-center">
         <h1>Cart is Empty</h1>
+        <div>
+          <img
+            src={image}
+            alt=""
+            className="w-52 rounded-sm"
+          />
+        </div>
       </div>
     </>
   );
